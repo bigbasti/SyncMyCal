93989217406853de750b807cf7db9f82b55bb7c7
Sebastian Gross <bigbasti@gmail.com>
2014-12-10 16:52:37 +0100

    Don't start another instance if there is already one instance of SyncMyCal running

--- SyncMyCal/Program.cs
+++ SyncMyCal/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,11 @@ namespace SyncMyCal
         [STAThread]
         static void Main()
         {
+            //Check if SyncMyCal is already running -> if so, exit
+            if (Process.GetProcessesByName(Application.ProductName).Length > 1)
+            {
+                Environment.Exit(1);
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmSettings());
